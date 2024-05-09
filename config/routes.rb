6f51a6c4aea3dddf90240501dd7f5b8dda821650
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-05-10 00:40:36 +0530

    feat: added routes and sed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'students/index'
-  get 'students/show'
-  get 'students/edit'
-  get 'students/new'
+  # get 'students/index'
+  # get 'students/show'
+  # get 'students/edit'
+  # get 'students/new'
+
+  resources :students
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
